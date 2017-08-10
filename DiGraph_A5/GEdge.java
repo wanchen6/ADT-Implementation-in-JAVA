51c8ba4f7834e36dbf304a9c352aea808d9d2469
wanchen6 <mengmeng306@yahoo.com>
2017-08-10 19:28:37 -0400

    Create GEdge.java

---
+++ DiGraph_A5/GEdge.java
@@ -0,0 +1,25 @@
+package DiGraph_A5;
+
+public class GEdge {
+	long id;
+	String sLabel;  //the label of source node
+	String dLabel;  //the label of destination node
+	long weight;
+	String eLabel;  //the label of the edge
+	GEdge next;
+	
+	public GEdge(long idNum, String sLabel, String dLabel) {
+		id = idNum;
+		this.sLabel = sLabel;
+		this.dLabel = dLabel;
+		weight = 1;
+		eLabel = null;
+		next = null;
+	}
+
+	public String getsLabel() {return sLabel;}
+	public String getdLabel() { return dLabel; }
+	public long getWeight() { return weight; }
+	public String geteLabel() { return eLabel; }
+	public GEdge getNext() { return next; }
+}
