eedb315b99c4cf92950d1b0f5f29fb6f1ac3b2c5
wanchen6 <mengmeng306@yahoo.com>
2017-08-10 19:30:43 -0400

    Create ShortestPathInfo.java

---
+++ Dijkstra_A6/ShortestPathInfo.java
@@ -0,0 +1,37 @@
+package Dijkstra_A6;
+
+public class ShortestPathInfo {
+/*
+ * 
+ * This class is to represent a single shortest path 
+ * from a source Node to a destination Node
+ * 
+ * Description of each field you are to populate:
+ * 
+ * String dest: the label of the destination node
+ * long totalWeight: the sum of the edge weights on the shortest path 
+ *                   from source node to destination node
+ * 
+ * Note on totalWeight: If a path from source node to the destination node 
+ *                      does not exist, set totalWeight to -1.
+*/
+  private String dest;
+  private long totalWeight;
+  
+  public ShortestPathInfo(String dest, long totalWeight){
+    this.dest=dest;
+    this.totalWeight=totalWeight;
+  }
+
+  public String getDest() {
+    return dest;
+  }
+
+  public long getTotalWeight() {
+    return totalWeight;
+  }
+  
+  public String toString(){
+    return "dest: "+dest+"\ttotalWeight: "+totalWeight;
+  }
+}
